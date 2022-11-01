e750e81ab39b0883af38278736b28c787116ce59
cinthia <carizono@gmail.com>
2022-11-01 14:37:07 -0300

    Aprimoramento do jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,5 +1,5 @@
 print("#############################################")
-print("Olá! Seja bemm vindo ao jogo de adivinhação!")
+print("Olá! Seja bem vindo ao jogo de adivinhação!")
 print("#############################################")
 
 numero_secreto = 42
@@ -8,20 +8,26 @@ total_de_tentativas = 3
 
 while (rodada <= total_de_tentativas):
     print("Tentativa ", rodada, "de ", total_de_tentativas)
-    chute_str = input("Digite um numero:")
+    chute_str = input("Digite um numero entre 1 e 100:")
     chute = int(chute_str)
-    print("O numero que você digitou é: ", chute)
+    rodada = rodada + 1
+    print("O número que você digitou é: ", chute)
+
+    if (chute < 1 or chute > 100):
+        print("Número inválido. Digite um número entre 1 e 100.")
+        continue
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
-    rodada = rodada + 1
 
     if (acertou):
         print("Você acertou!")
         break
-    elif (maior):
-        print("Você errou! O numero que você digitou é maior do que o numero secreto.")
-    elif (menor):
-        print("Você errou! O numero que você digitou é menor do que o numero secreto.")
-print("Fim do jogo")
+    else:
+        if (maior):
+            print("Você errou! O numero que você digitou é maior do que o numero secreto.")
+        elif (menor):
+            print("Você errou! O numero que você digitou é menor do que o numero secreto.")
+    print("Fim do jogo")
 
