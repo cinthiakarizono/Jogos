b050b8dd86a5d14bf1c3126fe3a2a4fe37536f8d
cinthia <carizono@gmail.com>
2022-11-02 15:17:23 -0300

    Adicionando jogo da forca

---
+++ forca.py
@@ -0,0 +1,19 @@
+def jogar():
+
+    print("###########################################")
+    print("Olá! Seja bem vindo ao jogo da forca")
+    print("###########################################")
+    palavra_secreta = "banana"
+
+    letra = str(input("Digite uma letra:"))
+    chute = []
+
+
+
+    print(chute)
+
+
+
+    print("Fim do jogo!")
+if (__name__ == "__main__"):
+    jogar()
